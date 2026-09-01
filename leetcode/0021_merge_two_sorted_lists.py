e496b75d1199e8c8fa6577f5846085357528f43a
josoriov <jfosorio.dev@gmail.com>
2026-09-01 12:07:45 -0500

    Adding practice scaffold for leetcode problem 21

---
+++ leetcode/0021_merge_two_sorted_lists.py
@@ -0,0 +1,21 @@
+"""
+LeetCode 21. Merge Two Sorted Lists
+
+Approach:
+- TBD
+
+Time: TBD
+Space: TBD
+"""
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def mergeTwoLists(list1: ListNode, list2: ListNode) -> ListNode:
+    """
+    TODO
+    """
+    pass
